f4f2926cafb4a4a3b69c29d5aceed536b34239df
ceconelli <guceconelli@hotmail.com>
2017-09-20 00:16:57 -0300

    fix instructions

--- Le-processeur.v
+++ Le-processeur.v
@@ -1,227 +1,214 @@
-module processador (DIN, Resetn, Clock, Run, Done, BusWires);
+module processor(DIN, Resetn, Clock, Run, Done);
 	input [15:0] DIN; //dado de entrada
 	input Resetn, Clock, Run;
 	output Done; //sinal que informa se a instruçao ja terminou
-	output [15:0] BusWires;
 	
-	wire [15:0]Rdata[7:0];//dados dos registradores
-	wire [15:0]Gdata;//dado do registrador G
-	wire [15:0]Adata;//dado do registrador A
-	wire [15:0]saida_ula;//valor calculado pela ULA
-	wire [1:0]estagio_atual;//estagio do processamento da instrução
-	reg [7:0]Rout;//sinal de controle do mux. De qual registrador virá a saida do mux
-	reg [7:0]Rin;// sinal de controle dos registradores. Condiciona se ocorrerá a escrita neles
-	wire [3:0]I;// armazena a instrução(4 primeiros bits)
-	wire [0:9]IR;//instrução
-	reg IRin;
-	reg DINout;
-	reg Ain;
-	reg Gout;
-	reg Gin;
-	reg [0:15] InstructionMemory [15:0];
-	reg [0:15] RegisterBank [7:0];
-	reg [0:15] DataMemory[7:0];
-	reg [3:0]pc;
-	reg [15:0]instruction;
-	reg [3:0] instructionIndex;
-	reg [2:0] registerA;
-	reg [2:0] registerB;
-	reg [5:0] imediate;
+	reg [15:0] InstructionMemory [15:0]; //Memória de instruções
+	reg [15:0] RegisterBank [7:0]; // Banco de registradores
+	reg [15:0] DataMemory [7:0]; // Memória de dados
+	reg [3:0]  pc; // index da memória de instruções
+	reg [1:0]  curr_stage; // estagio da instrução
+	reg [15:0] instruction; // instrução 16 bits
+	reg [3:0]  instructionIndex;// índice da instrução a ser executada
+	reg [2:0]  reg_A;
+	reg [2:0]  reg_B;
+	reg [5:0]  imediate;
 	reg [15:0] tmpreg;
 	reg [15:0] Gtmp;
-	reg [8:0] imd;
+	reg [15:0] memory_data;
+
+	output reg [15:0]out;
 
 	initial begin
 		
-		InstructionMemory[0] = 16'b0000001000000001;
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		InstructionMemory[i] =
-		pc = 0; 
+		InstructionMemory[0]  = 16'b0000_001_010_000000;//mv  R1,R2  | R.E = 2
+		InstructionMemory[1]  = 16'b0001_010_000_000101;//mvi R2,5	 | R.E = 5
+		InstructionMemory[2]  = 16'b0010_010_011_000000;//add R2,R3  | R.E = 8
+		InstructionMemory[3]  = 16'b0000_001_000_000001;//sub
+		InstructionMemory[4]  = 16'b0000_001_000_000001;//and
+		InstructionMemory[5]  = 16'b0000_001_000_000001;//slt
+		InstructionMemory[6]  = 16'b0000_001_000_000001;//sll
+		InstructionMemory[7]  = 16'b0000_001_000_000001;//srl
+		InstructionMemory[8]  = 16'b0000_001_000_000001;//mvnz
+		InstructionMemory[9]  = 16'b0000_001_000_000001;//ld
+		InstructionMemory[10] = 16'b0000_001_000_000001;//sd
+		InstructionMemory[11] = 16'b0000_001_000_000001;
+		InstructionMemory[12] = 16'b0000_001_000_000001;
+		InstructionMemory[13] = 16'b0000_001_000_000001;
+		InstructionMemory[14] = 16'b0000_001_000_000001;
+		InstructionMemory[15] = 16'b0000_001_000_000001;	
 
-		for(i=0;i<8;i=i+1) begin
+		for(i=0;i<16;i=i+1) begin
 			RegisterBank[i] = i;
-		end
+			DataMemory[i] = i;
+		end	
 		
+		pc = 4'b0000; 
+		Done = 1'b0;
+		curr_stage = 2'b00;
 	end
 
 	wire Clear = Done | ~Resetn | ~Run ;
-	contador estagio(Clear, Clock,estagio_atual);
-	assign I = IR[0:3];
-	dec3to8 decX (IR[4:6], 1'b1, registradorX);
-	dec3to8 decY (IR[7:9], 1'b1, registradorY);
+	contador stage(Clear, Clock,curr_stage);
 	
-	always @(/*estagio_atual or I or registrador1 or registrador2*/Clock or Done) begin
-			//... specify initial values
+	always @(posedge Clock or Done) begin
+			//Adicionar Run
 
 			if(Done) begin
 				pc = pc + 1'b0;
+				Done = 1'b0;
+				if (pc == 4'b1111) begin
+					pc = 4'b0000;
+				end
 			end
 
-			case (estagio_atual)
+			case (curr_stage)
 				2'b00: // busca instrução na memória
 				begin
-					//IRin = 1'b1;
 					instruction = InstructionMemory[pc];
-					instructionIndex = instruction[0:3];
-					registerA = instruction[4:6];
-					registerB = instruction[7:9];
-					imediate = instruction[10:15];
+					instructionIndex = instruction[15:12];
+					reg_A = instruction[11:9];
+					reg_B = instruction[8:6];
+					imediate = instruction[5:0];
 				end
+
 				2'b01: //define signals in time step 1
-				case (I)
+				case (instructionIndex)
 					4'b0000: //mv
 					begin
-						RegisterBank[registerA] = RegisterBank[registerB];
+						RegisterBank[reg_A] = RegisterBank[reg_B];
 						Done = 1'b1;
+						out = RegisterBank[reg_A];
 					end
 					
 					4'b0001: //mvi
 					begin
-						RegisterBank[registerA] = {registerB,imediate};
+						RegisterBank[reg_A] = {reg_B,imediate};
 						Done = 1'b1;
+						out = RegisterBank[reg_A];
 					end
 					
 					4'b0010: //add
 					begin
-						tmpreg = RegisterBank[registerA];
+						tmpreg = RegisterBank[reg_A];
 					end
 
-					4'b0100: // sub 
+					4'b0011: // sub 
 					begin
-						tmpreg = RegisterBank[registerA];
+						tmpreg = RegisterBank[reg_A];
 					end
 
-					4'b0101: 
+					4'b0100: //and 
 					begin
-						Done = 1'b0;
+						RegisterBank[reg_A] = RegisterBank[reg_A] & RegisterBank[reg_B];
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
+
+					4'b0101: //slt
+					begin
+						if (RegisterBank[reg_A] < RegisterBank[reg_B])
+							RegisterBank[reg_A] = 1;
+						else 
+							RegisterBank[reg_A] = 0;
+
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
+
+					4'b0110: //sll 
+					begin
+						RegisterBank[reg_A] = RegisterBank[reg_A] << RegisterBank[reg_B];
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
+
+					4'b0111: //srl
+					begin
+						RegisterBank[reg_A] = RegisterBank[reg_A] >> RegisterBank[reg_B];
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
+
+					4'b1000: //mvnz
+					begin
+					//TODO	
 
 					end
-					4'b0110: 
-					begin
-						Done = 1'b0;
 
-					end
-					4'b0111: 
+					4'b1001: //ld 
 					begin
-						Done = 1'b0;
+						tmpreg = RegisterBank[reg_B];
+					end
 
-					end
-					4'b0000: 
+					4'b1010: //sd 
 					begin
-						Done = 1'b0;
-
-					end
-					4'b0000: 
-					begin
-						Done = 1'b0;
-
-					end
-					4'b0000: 
-					begin
-						Done = 1'b0;
-
-					end
-					4'b0000: 
-					begin
-						Done = 1'b0;
+						tmpreg = RegisterBank[reg_A];
 
 					end
 				endcase
 				2'b10: //define signals in time step 2
-				case (I)
-					4'b0010:
+				case (instructionIndex)
+					4'b0010: //add
 					begin
 						Gtmp = tmpreg + RegisterBank[registerB];
 					end
 
-					4'b0100:
+					4'b0011://sub
 					begin
 						Gtmp = tmpreg - RegisterBank[registerB];
 					end
 
+					4'b1001: //ld
+					begin
+						RegisterBank[reg_A] = DataMemory[tmpreg];
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
+
+					4'b1010: //sd
+					begin
+						RegisterBank[reg_A] = DataMemory[tmpreg];
+						Done = 1'b1;
+						out = RegisterBank[reg_A];
+					end
 				
 				endcase
+
 				2'b11: //define signals in time step 3
-				case (I)
-					4'b0010:
+				case (instructionIndex)
+					4'b0010://add
 					begin
-						RegisterBank[registerA] = Gtmp;
+						RegisterBank[reg_A] = Gtmp;
 						Done = 1'b1;
+						out = RegisterBank[reg_A];
 					end
 
-					4'b0100:
+					4'b0011://sub
 					begin
-						RegisterBank[registerA] = Gtmp;
+						RegisterBank[reg_A] = Gtmp;
 						Done = 1'b1;
+						out = RegisterBank[reg_A];
 					end
-
 				endcase
 			endcase
-	end
-	regn reg_0 (BusWires, Rin[0], Clock, R0);
-	regn reg_1 (BusWires, Rin[1], Clock, R1);
-	//... instantiate other registers and the adder/subtracter unit
-	//... define the bus
+	end	
 endmodule
 
-module contador(Clear, Clock, Q);
+module contador(Clear, Clock,out);
 	input Clear, Clock;
-	output reg [1:0] Q;
+	output reg [1:0] out;
 	
 	always @(posedge Clock)begin
 		if (Clear)
-			Q <= 2’b0;
+			out <= 2'b00;
 		else
-			Q <= Q + 1’b1;
+			out <= out + 1'b1;
 	end 
 	
 endmodule
 
-module dec3to8(W, En, Y);
-	input [2:0] W;
-	input En;
-	output [0:7] Y;
-	reg [0:7] Y;
-	
-	always @(W or En) begin
-		if (En == 1)
-			case (W)
-			3'b000: Y = 8'b10000000;
-			3'b001: Y = 8'b01000000;
-			3'b010: Y = 8'b00100000;
-			3'b011: Y = 8'b00010000;
-			3'b100: Y = 8'b00001000;
-			3'b101: Y = 8'b00000100;
-			3'b110: Y = 8'b00000010;
-			3'b111: Y = 8'b00000001;
-			endcase
-		else
-			Y = 8'b00000000;
-		end
-endmodule
+module toplevel(SW,LEDR,HEX0);
 
-module regn(R, Rin, Clock, Q);
-	parameter n = 16;
-	input [n-1:0] R;
-	input Rin, Clock;
-	output [n-1:0] Q;
-	reg [n-1:0] Q;
-	
-	always @(posedge Clock)
-		if (Rin)
-			Q <= R;
 
 endmodule
